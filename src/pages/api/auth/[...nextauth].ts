1a31830ab761a641046899ea5b5bea0e9dcf47dd
danielbcarvalho <danielborgesdecarvalho@gmail.com>
2021-04-14 23:55:32 -0300

    create callback auth and add  user to fauna users collection

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { query as q } from "faunadb";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,7 +14,25 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-
-  // A database is optional, but required to persist accounts in a database
-  database: process.env.DATABASE_URL,
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
