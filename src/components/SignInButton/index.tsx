63beba337597e60175b6d90ccc0d79f1fb3d7283
danielbcarvalho <danielborgesdecarvalho@gmail.com>
2021-04-14 21:41:57 -0300

    add faunadb connection

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -8,20 +8,6 @@ import styles from "./styles.module.scss";
 export function SignInButton() {
   const [session, loading] = useSession();
 
-  //const isUserLoggedIn = true;
-  // return isUserLoggedIn ? (
-  //   <button type="button" className={styles.signInButton}>
-  //     <FaGithub color="#04d361" />
-  //     Daniel Carvalho
-  //     <FiX color="#737380" className={styles.closeIcon} />
-  //   </button>
-  // ) : (
-  //   <button type="button" className={styles.signInButton}>
-  //     <FaGithub color="#ebA417" />
-  //     Sign In with Github
-  //   </button>
-  // );
-
   return (
     <>
       {!session && (
